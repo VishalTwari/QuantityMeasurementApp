6924e2927355b1949573ef134ae4f3da0a1ee016
VI20595625 <vishal.tiwari@wipro.com>
2026-02-02 12:09:56 +0530

    QuantityMeasurement test

--- src/com/QualityMeasurementApp.java
+++ src/com/QualityMeasurementApp.java
@@ -2,29 +2,19 @@ package com;
 
 public class QualityMeasurementApp {
     public static void  main(String[] args) {
-        demonstrateFeetEquality();
-        demonstrateInchesEquality();
-        demonstrateFeetInchesComparison();
-    }
-    public static boolean demonstrateLengthEquality(Length l1,Length l2){
-        return l1.equals(12);
-    }
-    public static void  demonstrateFeetEquality() {
         Length lenght1 = new Length(1.0, LengthUnit.FEET);
         Length lenght2 = new Length(1.0,LengthUnit.FEET);
-        System.out.println("Feet equality : " + demonstrateLengthEquality(lenght1,lenght2));
-            }
-            public static void demonstrateInchesEquality()
-            {
-                Length length3=new Length(12.0,LengthUnit.INCH);
-                Length length4=new Length(12.0,LengthUnit.INCH);
-                System.out.println("Inches equality :"+ demonstrateLengthEquality(length3,length4));
+        System.out.println("Feet equality : " + lenght1.equals(lenght2));
 
-            }
-public static void demonstrateFeetInchesComparison()
-{
-    Length length1=new Length(1.0,LengthUnit.FEET);
-    Length length2=new Length(12.0,LengthUnit.INCH);
-    System.out.println("FEET & INCHES comparision : " + demonstrateLengthEquality(length1,length2));
-}
+        Length length3=new Length(12.0,LengthUnit.INCH);
+        Length length4=new Length(12.0,LengthUnit.INCH);
+        System.out.println("inces equality : " + length3.equals(length4));
+
+        Length length5=new Length(12.0,LengthUnit.INCH);
+        Length length6=new Length(11.0,LengthUnit.INCH);
+        System.out.println("inces equality : " + length5.equals(length6 ));
+
+
+    }
+
 }
--- src/com/QualityMeasurementAppC3Test.java
+++ src/com/QualityMeasurementAppC3Test.java
@@ -1,11 +1,13 @@
 package com;
 //package com.QualityMeasurementAppTest;
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-public class QualityMeasurementAppTest {
-  //  @test
+@Nested
+class QualityMeasurementAppTest {
+   @Test
     public void testFeetEquality() {
         Length length1=new Length(1.0,LengthUnit.FEET);
         Length length2=new Length(1.0,LengthUnit.FEET);
@@ -15,44 +17,39 @@ public class QualityMeasurementAppTest {
     private void assertEquals(Length length1, Length length2) {
     }
 
-    //@Test
+    @Test
     public void testInchesEquality(){
         Length length1 = new Length(12.0,LengthUnit.INCH);
         Length length2 = new Length(12.0,LengthUnit.INCH);
         assertEquals(length1,length2);
     }
-    //@Test
+    @Test
     public void testFeetInequality(){
         Length length1=new Length(1.0,LengthUnit.FEET);
         Length length2 = new Length(12.0,LengthUnit.INCH);
         assertEquals(length1,length2);
     }
-    //@Test
-    /*public void testFeetInequality() {
-        Length length1 = new Length(1.0,LengthUnit.FEET);
-        Length length2 = new Length(2.0,LengthUnit.FEET);
-        assertNotEquals(length1,length2);
-    }
-*/
-    private void assertNotEquals(Length length1, Length length2) {
-    }
 
-    //@Test
+
+
+   @Test
     public void testInchesInequality() {
         Length length1 = new Length(10.0,LengthUnit.INCH);
         Length length2 = new Length(2.0,LengthUnit.INCH);
         assertNotEquals(length1,length2);
     }
-    //@Test
+    @Test
     public void testCrossUnitInequality(){
         Length length1 = new Length(1.0,LengthUnit.FEET);
         Length length2 = new Length(10.0,LengthUnit.INCH);
         assertNotEquals(length1,length2);
     }
-   // @Test
+    @Test
     public void testMultipleFeetComparison(){
         Length length1=new Length(2.0,LengthUnit.FEET);
         Length length2=new Length(24.0,LengthUnit.INCH);
         assertEquals(length1,length2);
     }
-}
+
+    }
+
