871789e91925a056d19b477b17589c26b668f10d
VI20595625 <vishal.tiwari@wipro.com>
2026-02-11 17:55:05 +0530

    UC6 Junit test

--- src/Test/QuantityMeasurementAppTest.java
+++ src/Test/QuantityMeasurementAppTest.java
@@ -1,4 +1,4 @@
-//package com.apps.quantitymeasurement;
+//package Test;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class QuantityMeasurementAppTest {
